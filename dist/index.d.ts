06fa3f0bfb52c4fadf2ccbe45f86c9eef871520c
hornta <hornta@protonmail.com>
2022-10-18 15:49:36 +0200

    build distributable package

---
+++ dist/index.d.ts
@@ -0,0 +1,65 @@
+export declare enum PlanStatus {
+    InProgress = "in_progress",
+    Done = "done"
+}
+export declare type Plan = {
+    id: string;
+    name: string;
+    description: string;
+    periodFrom: string;
+    periodTo: string;
+    basedOnPlanId: number;
+    legalEntityId: string;
+    actualsFrom: string;
+    status: PlanStatus;
+    rootTaskId: string;
+    createdAt: string;
+    comparisonPlans: {
+        planId: string;
+        taskId: string;
+    }[];
+    createTemplatesForRegisters: boolean;
+    createTasksForCostCenters: boolean;
+    createScenariosFromActuals: boolean;
+    createScenariosPerTask: boolean;
+    basedOnYear: number;
+    periodLength: number;
+    companyId: string;
+    generateIbBasedOnOb: boolean;
+    thousands: boolean;
+};
+export declare type OrganizationalUnit = {
+    id: string;
+    name: string;
+    code: string;
+};
+export declare enum TaskStatus {
+    InProgress = "in_progress",
+    ReadyForReview = "ready_for_review",
+    Approved = "approved"
+}
+export declare type Task = {
+    id: string;
+    name: string;
+    description: string;
+    targetType: string;
+    targetAmount: number;
+    owner: string;
+    ownerMd5: string;
+    assignedTo: string | null;
+    assignedToMd5: string | null;
+    dueDate: string;
+    distributable: boolean;
+    organizationalUnits: OrganizationalUnit[];
+    versionId: string;
+    versionName: string;
+    status: TaskStatus;
+    active: boolean;
+    rootTaskId: string;
+} & ({
+    parentId: null;
+    parentStatus: null;
+} | {
+    parentId: string;
+    parentStatus: TaskStatus;
+});
---
+++ dist/index.js
@@ -0,0 +1,11 @@
+export var PlanStatus;
+(function (PlanStatus) {
+    PlanStatus["InProgress"] = "in_progress";
+    PlanStatus["Done"] = "done";
+})(PlanStatus || (PlanStatus = {}));
+export var TaskStatus;
+(function (TaskStatus) {
+    TaskStatus["InProgress"] = "in_progress";
+    TaskStatus["ReadyForReview"] = "ready_for_review";
+    TaskStatus["Approved"] = "approved";
+})(TaskStatus || (TaskStatus = {}));
