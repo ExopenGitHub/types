53e5096513615a2513a0ed685d90621836d5474e
Marcus Ahlberg <m@rcusahlberg.se>
2023-03-15 16:13:39 +0100

    fix issues

--- src/common/bigIntAsStringSchema.ts
+++ src/common/bigIntAsStringSchema.ts
@@ -1,3 +1,6 @@
 import { z } from "zod";
 
-export const bigIntAsStringSchema = z.bigint().transform((id) => id.toString());
+export const bigIntAsStringSchema = z
+  .bigint()
+  .or(z.string())
+  .transform((id) => id.toString());
